b8cb606958af7052fe5d00b2241feb1bca725151
reaitten <wsy0xf2u8@relay.firefox.com>
2022-02-13 09:43:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # Install rclone static binary
 wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 mkdir $PWD/rclone-linux/
-unzip -q rclone-current-linux-amd64.zip -d $PWD/rclone-linux/ -j
+unzip -q -j rclone-current-linux-amd64.zip -d $PWD/rclone-linux/
 export PATH=$PWD/rclone-linux:$PATH
